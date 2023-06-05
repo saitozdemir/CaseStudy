e391f2aa84b03324f709d306a14cfa8870f575f3
saitozdemir <saitozdemir@icloud.com>
2023-06-05 15:24:54 +0300

    edit

--- CaseStudy/Controllers/HomeController.cs
+++ CaseStudy/Controllers/HomeController.cs
@@ -1,13 +1,7 @@
 ﻿using CaseStudy.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System;
-using System.Data;
-using System.Diagnostics;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 namespace CaseStudy.Controllers
 {
